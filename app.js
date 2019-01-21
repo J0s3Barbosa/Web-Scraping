704fab540453ea2d510b2eab6458b6a3cd4c4d84
appchto <appchto@gmail.com>
2019-01-21 16:27:21 -0200

    6

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const http = require('http');
 var dt = require('./datetime');
 
-const PORT = 3000;
-
   http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(req.url+"<p>");
     res.write("The date and time are currently: " + dt.myDateTime());
 
     res.end();
-  }).listen(PORT);
+  }).listen(3000);
 
 
 
