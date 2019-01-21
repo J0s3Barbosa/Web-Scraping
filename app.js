c14ecd5016abe5a0d2e6f0ba94807ac8a473be36
appchto <appchto@gmail.com>
2019-01-21 16:12:50 -0200

    4

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-const rp = require('request-promise');
-const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 const http = require('http');
 var dt = require('./datetime');
 
-const PORT = 8080;
-const html =  "";
-
-rp(url)
-.then(function(html){
-  //success!
-  console.log(html);
-})
-.catch(function(err){
-  //handle error
-});
+const PORT = 3000;
 
   http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
