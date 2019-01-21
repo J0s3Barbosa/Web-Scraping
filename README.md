938b13dff067827c799724d80dcd2ff32c5b86bf
appchto <appchto@gmail.com>
2019-01-21 15:59:24 -0200

    3

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Web-Scraping
+
+https://medium.freecodecamp.org/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3
+
+
+
+# Para execucao do projeto:
+# Install dependencies:
+$ npm install
+
+# Start the server:
+$ npm start
+
+http://localhost:8080/
+
--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const scraper = require('./scraper');
-const fs = require('fs');
-const url = 'http://imgur.com/gallery/hj4NW';
-const path = 'text.txt'
+const rp = require('request-promise');
+const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
+const http = require('http');
+var dt = require('./datetime');
 
-// Callback example
-// scraper.imgScrape(url, (data) => {
-//   console.log('data from scraper received');
-//   console.log(data);
-// })
+const PORT = 8080;
+const html =  "";
 
-// Promise example
-scraper.imgScrape2(url)
-  .then((data) => {
-    console.log('data from scraper received ');
-    fs.writeFile(path, JSON.stringify(data), (error) => {
-      if(error) {
-        console.log(error);
-      }
-      console.log('Successfully wrote to ' + path);
-    })
-  })
-  .catch((error) => {
-    console.log('error scraping data');
-  })
+rp(url)
+.then(function(html){
+  //success!
+  console.log(html);
+})
+.catch(function(err){
+  //handle error
+});
+
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(req.url+"<p>");
+    res.write("The date and time are currently: " + dt.myDateTime());
+
+    res.end();
+  }).listen(PORT);
+
+
+
+ 
