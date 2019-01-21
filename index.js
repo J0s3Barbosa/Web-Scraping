8f27ebd119676b30cb252a304688bb1e5094a38d
appchto <appchto@gmail.com>
2019-01-21 20:38:36 -0200

    +

--- index.js
+++ index.js
@@ -7,21 +7,20 @@ const PORT = process.env.PORT || 5000
 
 mongoose.Promise = global.Promise;
 var dbmlabUrl = 'mongodb://appchto:Password!1@ds237574.mlab.com:37574/node'
-mongoose.connect(dbmlabUrl ,{ useNewUrlParser: true });
+mongoose.connect(dbmlabUrl, { useNewUrlParser: true });
 var connection = mongoose.connection;
 
 connection.on("open", function (err) {
   if (err) {
-      console.log("Error on connectiong " + err); // it will print your collection data
+    console.log("Error on connectiong " + err); // it will print your collection data
   }
   console.log("mongodb is connected!!");
 });
-var app = express();
-
-app.use(express.static(path.join(__dirname, 'public')))
-app .set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
-app .get('/cr', (req, res) => res.render('pages/indexcr'))
-app .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cr', (req, res) => res.render('pages/indexcr'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
   // app.use('/c', clashRoutes);
