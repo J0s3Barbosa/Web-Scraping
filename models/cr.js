a83b125dfd8bd6eaaaf72e4271a8662c6b97bbd3
appchto <appchto@gmail.com>
2019-02-23 00:50:40 -0300

    backend ok clash

--- models/cr.js
+++ models/cr.js
@@ -1,25 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-  var ClashSchema = new Schema({
+var ClashSchema = new Schema({
+  Name: {
+    type: String,
+    required: true
+  },
+  Started_Trophies: {
+    type: Number,
+    required: true
+  },
+  Trophies: {
+    type: Number,
+    required: true
+  },
+  Victory: {
+    type: Number,
+    required: true
+  },
+  Defeat: {
+    type: Number
+  },
+  Total_Trophies: {
+    type: Number
 
-    Trophies: {
-        type: Number,
-        required: true
-    },
-    Victory: {
-        type: Number,
-        required: true
-    },
-    Defeat: {
-        type: Number
-    },
-    Played_at: {
-        type: Date,
-      default: Date.now
-    }
+  },
+  Played_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 // module.exports = ClashSchema;
-module.exports = mongoose.model('clashroyale', ClashSchema)
-
+module.exports = mongoose.model("clashroyale", ClashSchema);
--- routes/clashRoutes.js
+++ routes/clashRoutes.js
@@ -10,6 +10,7 @@ router.get('/clash/:id', clashController.clashroyale_detail);
 router.post('/clash/createmethod', verifyToken, clashController.clashroyale_createMethod_post);
 router.put('/clash/:id', verifyToken,  clashController.clashroyale_update_post);
 router.delete('/clash/:id', verifyToken, clashController.clashroyale_delete_post);
+router.get('/clashs/:orderby', clashController.clashroyale_OrderBy);
 
 router.get('/clashroyaleapi', clashController.clashroyaleapi);
 
