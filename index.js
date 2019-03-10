08457ca8119a9bb0c4da0ad3f093a6d21a52e60c
appchto <appchto@gmail.com>
2019-03-09 22:06:06 -0300

    +

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ const morgan = require("morgan");
 
 
 var clashRoutes = require('./routes/clashRoutes');
+var clashApiRoutes = require('./routes/clashApiRoutes');
 var weatherRoutes = require('./routes/weatherRoutes');
-var indexRouters = require('./routes/indexRouters');
 var users = require('./routes/users');
 
 const { fork } = require('child_process');
 
 const PORT = process.env.PORT || 5000
 const API_PATH = '/api/v1'
+const APP_PATH = '/app'
 
 require('./config/passport')(passport);
 
@@ -88,10 +89,9 @@ app.use(express.static(path.join(__dirname, 'public')))
     user : req.user
   }))
   
-  .use(API_PATH + '/clashRoyale', clashRoutes)
-  .use(API_PATH + '/weather', weatherRoutes)
-  .use(API_PATH + '/youtube', indexRouters)
-  .use(API_PATH + '/default', indexRouters)
+  .use(APP_PATH + '/clashRoyale', clashRoutes)
+  .use(API_PATH + '/clashRoyale', clashApiRoutes)
+  .use(APP_PATH + '/weather', weatherRoutes)
   .use('/users', users )
 
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
