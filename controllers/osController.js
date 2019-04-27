9a314a36040e7556c0b5f0d402312992dc5f4378
appchto <appchto@gmail.com>
2019-04-27 01:45:49 -0300

    +

--- controllers/osController.js
+++ controllers/osController.js
@@ -29,38 +29,45 @@ exports.getAll = function (req, res) {
   });
 };
 
-exports.getOsById = function (req, res) {
+exports.search = function (req, res) {
 
-  var myfind = req.query.id_OS;
+  var id_OS = req.query.id_OS;
+  var os_status = req.query.os_status;
+  var myfind = null;
 
-   if (
-    myfind == null ||
-    myfind == undefined ||
-    myfind.length == 0 
+
+  if ((id_OS == (null || undefined || '')
+    || os_status == (null || undefined || ''))
   ) {
-  
-        let message = 'You need to pass the OS number!'
+
+    let message = 'You need to pass the Param to be searched!'
     res.json({
       message
     });
   }
- else if (myfind != undefined || myfind != null) {
-      myfind = { id_OS: myfind };
 
-      Os.find(myfind, (err, os ) => {
-        if (err) {
-          res.send(err);
-        }
-        if ( os.length == 0) {
-          let message = 'No OS found!'
-          res.send({message});
+  else {
+    if (id_OS != (null || undefined)) {
+      myfind = { id_OS };
+    }
+    else if (os_status != (null || undefined)) {
+      myfind = { os_status };
+    }
+    Os.find(myfind, (err, os) => {
+      if (err) {
+        res.send(err);
       }
-      else{
+      if (os.length == 0) {
+        let message = 'Didnt find any OS!'
+        res.send({ message });
+      }
+      else {
         res.json(os);
       }
-      }).catch(function (err) {
-        res.send(err);
-      });
+    }).catch(function (err) {
+      res.send(err);
+    });
+
 
   }
 
@@ -70,42 +77,42 @@ exports.getOsByStatus = function (req, res) {
 
   var myfind = req.query.os_status;
 
-   if (
+  if (
     myfind == null ||
     myfind == undefined ||
-    myfind.length == 0 
+    myfind.length == 0
   ) {
-  
-        let message = 'You need to pass os_status!'
+
+    let message = 'You need to pass os_status!'
     res.json({
       message
     });
   }
- else if (myfind != undefined || myfind != null) {
+  else if (myfind != undefined || myfind != null) {
 
 
-      myfind = { os_status: myfind };
+    myfind = { os_status: myfind };
 
-      Os.find(myfind, (err, os ) => {
-        if (err) {
-          res.send(err);
-        }
-        if ( os.length == 0) {
-          let message = 'Didnt find any status!'
-          res.send({message});
+    Os.find(myfind, (err, os) => {
+      if (err) {
+        res.send(err);
       }
-      else{
+      if (os.length == 0) {
+        let message = 'Didnt find any status!'
+        res.send({ message });
+      }
+      else {
         res.json(os);
       }
-      }).catch(function (err) {
-        res.send(err);
-      });
+    }).catch(function (err) {
+      res.send(err);
+    });
 
   }
 
 
 };
-exports.os_post = function(req, res) {
+exports.os_post = function (req, res) {
   try {
     let newOs = new Os(req.body);
 
@@ -125,13 +132,13 @@ exports.os_post = function(req, res) {
     res.json(error);
   }
 };
-exports.os_update = function(req, res) {
+exports.os_update = function (req, res) {
   try {
     Os.findByIdAndUpdate(
       req.params.id,
       req.body,
       { new: true },
-      function(err, os) {
+      function (err, os) {
         if (err) res.send(err);
         res.json(os);
       }
@@ -140,9 +147,9 @@ exports.os_update = function(req, res) {
     res.json(error);
   }
 };
-exports.os_delete = function(req, res) {
+exports.os_delete = function (req, res) {
   try {
-    Os.findByIdAndRemove(req.params.id, function(err, os) {
+    Os.findByIdAndRemove(req.params.id, function (err, os) {
       if (err) res.send(err);
       res.json(os);
     });
@@ -150,8 +157,38 @@ exports.os_delete = function(req, res) {
     res.json(error);
   }
 };
+exports.get_one = function (req, res) {
 
-exports.osInterface = function(req, res) {
+  var myfind = req.params.id;
+
+  if (myfind == null || myfind == undefined) {
+    let message = 'You need to pass the OS number!'
+    res.json({
+      message
+    });
+  }
+  else if (myfind != undefined || myfind != null) {
+    myfind = { _id: myfind };
+
+    Os.find(myfind, (err, os) => {
+      if (err) {
+        res.send(err);
+      }
+      if (os.length == 0) {
+        let message = 'No OS found!'
+        res.send({ message });
+      }
+      else {
+        res.json(os);
+      }
+    }).catch(function (err) {
+      res.send(err);
+    });
+
+  }
+
+};
+exports.osInterface = function (req, res) {
   try {
     res.render("pages/index_os", {
       user: req.user
--- routes/osRoutes.js
+++ routes/osRoutes.js
@@ -6,9 +6,10 @@ var osController = require('../controllers/osController');
 router.post('/os/new', osController.os_post);
 router.put('/os/:id',  osController.os_update);
 router.delete('/os/:id', osController.os_delete);
+router.get('/os/findbyid/:id', osController.get_one);
 router.get('/os/all', osController.getAll );
 router.get('/os', osController.default );
-router.get('/os/search', osController.getOsById );
+router.get('/os/search', osController.search );
 router.get('/os/searchStatus', osController.getOsByStatus );
 
 module.exports = router;
