5c699459c415f05969c7561155a0d298e1f90e53
appchto <appchto@gmail.com>
2019-07-02 00:14:59 -0300

    todo insert

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -7,22 +7,12 @@ const { ensureApiAuthenticated } = require('../config/authApi');
 var todoController = require('../controllers/todoController');
 
 router.get('/', ensureAuthenticated , todoController.index);
-router.post('/clash/createmethod', verifyToken, todoController.clashroyale_createMethod_post);
-router.put('/clash/:id', verifyToken,  todoController.clashroyale_update_post);
-router.delete('/clash/:id', verifyToken, todoController.clashroyale_delete_post);
-router.get('/clashsAllapi', verifyToken, todoController.getClashRoyaleList );
-
-router.get('/clashs', ensureApiAuthenticated, todoController.getClashRoyaleListOwner );
-router.get('/clash/:id', ensureAuthenticated, todoController.clashroyale_detail);
-
-
-router.get('/clashs/:orderby', ensureAuthenticated,  todoController.clashroyale_OrderBy);
-
-router.get('/clashroyaleapi', ensureAuthenticated , todoController.clashroyaleapi);
-
-router.get('/clashsAll', ensureAuthenticated, todoController.getClashRoyaleList );
-
-router.get('/ClashRoyaleClickAndGetPrint', todoController.ClashRoyaleClickAndGetPrint);
+router.get('/all', ensureAuthenticated , todoController.all);
+router.put('/update/:id', verifyToken,  todoController.update);
+router.delete('/delete/:id', verifyToken, todoController.delete);
+router.post('/post', verifyToken, todoController.post);
+router.get('/detail/:id', ensureAuthenticated, todoController.detail);
+router.get('/OrderBy/:orderby', ensureAuthenticated,  todoController.OrderBy);
 
 module.exports = router;
 
